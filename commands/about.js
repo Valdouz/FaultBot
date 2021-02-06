8dc0711aec4f70f3ec195d3fdd4cf976f210d04c
xokirBios <60980527+xokirBios@users.noreply.github.com>
2021-02-06 19:33:30 +0100

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -3,7 +3,7 @@ module.exports = {
     run: message => {
     message.channel.send(new Discord.MessageEmbed()
         .setTitle("<:019spy:753720260775116930> Informations sur " + `FaultBot `)
-        .addField('<:king:768073681942872105> __**Développeur :**__', `kiop#1988`, true)
+        .addField('<:king:768073681942872105> __**Développeur :**__', `kiop#9686`, true)
         .addField("📂 __Version de discord.js__ :", `v12.2.0`, true)
         .addField("__**Lien d'invitation :**__", "[**Cliquez ici**](https://discordapp.com/oauth2/authorize?client_id=739176121978847273&scope=bot&permissions=8)", true)
 
